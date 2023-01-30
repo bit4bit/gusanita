942406ed7a01492b03260cd0e062db27f06a5cb3
bit4bit <bit4bit@riseup.net>
2023-01-30 00:57:35 +0000

    refactor: extract method
    FossilOrigin-Name: bf0387810a1b4e64b9dea2fd479a7cd60a7ac0e7c00241b8f3fa4e3fdfcf8fc1

--- Gusanita.Test/ClassicGameTest.cs
+++ Gusanita.Test/ClassicGameTest.cs
@@ -8,7 +8,7 @@ public class ClassicGameTest
     [Test]
     public void player_earn_1_point_when_eat_a_banana()
     {
-        var player = new Player(x: 0, y: 0);
+        var player = aPlayer();
         var game = new Game(player, width: 5, height: 5);
         game.Plant(new BananaFruit(x: 1, y: 0));
 
@@ -21,7 +21,7 @@ public class ClassicGameTest
     [Test]
     public void player_earn_3_point_when_eat_a_banana()
     {
-        var player = new Player(x: 0, y: 0);
+        var player = aPlayer();
         var game = new Game(player, width: 5, height: 5);
         game.Plant(new PapayaFruit(x: 1, y: 0));
 
@@ -34,7 +34,7 @@ public class ClassicGameTest
     [Test]
     public void player_dead_if_eat_it_self()
     {
-        var player = new Player(x: 0, y: 0);
+        var player = aPlayer();
         var game = new Game(player);
         game.Plant(aPapaya(x: 1, y: 0));
         game.Plant(aPapaya(x: 2, y: 0));
@@ -58,7 +58,7 @@ public class ClassicGameTest
     [Test]
     public void player_dead_if_move_out_of_game()
     {
-        var player = new Player(x: 0, y: 0);
+        var player = aPlayer();
         var game = new Game(player);
 
         player.ToWest();
@@ -67,6 +67,12 @@ public class ClassicGameTest
         Assert.That(game.IsFinished, Is.EqualTo(true));
     }
 
+
+    private Player aPlayer(int x = 0, int y = 0)
+    {
+        return new Player(x: x, y: y);
+    }
+    
     private Fruitable aPapaya(int x, int y)
     {
         return new PapayaFruit(x: x, y: y);
