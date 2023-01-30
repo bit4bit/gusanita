0c0f4da5f548a18aa9abb61b6be03cc5cf0f8308
bit4bit <bit4bit@riseup.net>
2023-01-30 01:50:29 +0000

    feat: adicion archivo faltante
    FossilOrigin-Name: 6928b537eded5dd330c21ee93522678e4749cc9bfd2267906338df6713c71a9a

---
+++ Gusanita.ClassicGame/GameRandomBuilder.cs
@@ -0,0 +1,54 @@
+namespace Gusanita.ClassicGame;
+
+public class GameRandomBuilder
+{
+    int _width = 0;
+    int _height = 0;
+    int _fruits = 0;
+    Random _rnd = new Random();
+
+    public static GameRandomBuilder initialize()
+    {
+        return new GameRandomBuilder();
+    }
+    
+    public GameRandomBuilder width(int val)
+    {
+        _width = val;
+        return this;
+    }
+
+    public GameRandomBuilder height(int val)
+    {
+        _height = val;
+        return this;
+    }
+
+    public GameRandomBuilder fruits(int val)
+    {
+        _fruits = val;
+        return this;
+    }
+
+    public Game game(Player player, GusanitaBehavior? behavior = null)
+    {
+        var game = new Game(player, Wall.of(width: _width,
+                                            height: _height),
+                            behavior: behavior);
+        for(int i = 0; i <  _fruits; i++)
+        {
+            int x = _rnd.Next(1, _width);
+            int y = _rnd.Next(1, _height);
+
+            // frutas
+            var fruits = new List<Fruitable>{
+                new PapayaFruit(x: x, y: y),
+                new BananaFruit(x: x, y: y)
+            };
+            
+            game.Plant(fruits[_rnd.Next(fruits.Count)]);
+        }
+
+        return game;
+    }
+}
